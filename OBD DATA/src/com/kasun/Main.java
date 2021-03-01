299e5ce852f7db715b97425671d6ee27f0ef7902
kasun-7 <66461543+kasun-7@users.noreply.github.com>
2021-03-01 21:25:41 +0530

    Add files via upload

--- OBD DATA/src/com/kasun/Main.java
+++ OBD DATA/src/com/kasun/Main.java
@@ -1,42 +1,17 @@
 package com.kasun;
-
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.File;  // Import the File class
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-public class Main implements Load {
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("OBD.txt");
+        Scanner myReader = new Scanner(file);
 
-
-    public static void main(String[] args) {
-        List<LoadData> LoadData = new ArrayList<>();
-    }
-        @Override
-        public void LoadData() {
-
-            BufferedReader Reader = null;
-            try {
-                Reader = new BufferedReader(new FileReader("OBD.txt"));
-                String data;
-
-                while ((data = Reader.readLine()) != null) {
-                    System.out.println(data);
-                }
-            } catch (IOException save) {
-                save.printStackTrace();
-            }
-            try {
-                Reader.close();
-            } catch (IOException save) {
-                System.out.println("There was an error (!)");
-                save.printStackTrace();
-            }
-
-
+        while (myReader.hasNextLine()) {
+            System.out.println(myReader.nextLine());
         }
-	// write your code here
     }
 
+}
 
